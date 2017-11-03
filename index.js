f0d66a1f41c6378b7f88013f39266b9a356415d9
Filip Chalupa <chalupa.filip@gmail.com>
2017-11-04 00:27:42 +0100

    Observe and trigger

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ module.exports = class InView {
 
 	/*
 		options = {
-			element: document.querySelectorAll('.dot'),
+			elements: document.querySelectorAll('.dot'),
 			thresholds: [
 				{
 					threshold: 0, // 0 to 1
-					in: (element) => void,
-					out: (element) => void,
+					in: (element, entry) => void,
+					out: (element, entry) => void,
 					once: true,
 				},
 			],
@@ -17,31 +17,72 @@ module.exports = class InView {
 	*/
 
 	constructor(options) {
-		this.element = options.element
+		this.elements = options.elements
 		this.thresholds = this.parseThresholds(options)
+
+		this.observe(this.thresholds)
 	}
 
+
 	parseThreshold(data) {
-			if (data.threshold || data.in || data.out)
-			return {
-				threshold: data.threshold || 0,
-				in: data.in || (() => {}),
-				out: data.out || (() => {}),
-				once: data.once || false,
-			}
-			return null
+		if (data.threshold || data.in || data.out)
+		return {
+			threshold: data.threshold || 0,
+			in: data.in || (() => {}),
+			out: data.out || (() => {}),
+			once: data.once || false,
 		}
+		return null
+	}
+
 
 	parseThresholds(options) {
 		const thresholds = []
 
 		thresholds.push(this.parseThreshold(options))
 
-		options.thresholds.forEach((threshold) => {
-			thresholds.push(this.parseThreshold(threshold))
-		})
+		if (options.thresholds) {
+			options.thresholds.forEach((threshold) => {
+				thresholds.push(this.parseThreshold(threshold))
+			})
+		}
 
 		return thresholds.filter((threshold) => threshold !== null)
+	}
+
+
+	observe(thresholds) {
+		const observerOptions = {
+			threshold: [],
+		}
+
+		thresholds.forEach((threshold) => {
+			observerOptions.threshold.push(threshold.threshold)
+		})
+
+
+		const observer = new IntersectionObserver((entries) => {
+			entries.forEach((entry) => this.trigger(entry))
+		}, observerOptions)
+
+		this.elements.forEach((element) => {
+			observer.observe(element)
+		})
+	}
+
+
+	trigger(entry) {
+		const intersectionRatio = entry.intersectionRatio
+
+		this.thresholds.forEach((threshold) => {
+			let callback = null
+			if (intersectionRatio > threshold.threshold) {
+				callback = threshold.in
+			} else {
+				callback = threshold.out
+			}
+			callback.call(null, entry.target, entry)
+		})
 	}
 
 }
